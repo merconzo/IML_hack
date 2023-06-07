6e46ada9d86d3d4911038f6c6c63623b126a4a0d
merconzo <75909944+merconzo@users.noreply.github.com>
2023-06-07 22:34:23 +0300

    files

---
+++ task_1/code/hackathon_code/explore_data.py
@@ -0,0 +1,40 @@
+import pandas as pd
+import numpy as np
+import sklearn as sk
+from sklearn.model_selection import RandomizedSearchCV
+from sklearn.ensemble import RandomForestClassifier
+import plotly.express as px
+
+
+n_estimators = [int(x) for x in np.linspace(start=50, stop=2000, num=10)]
+max_features = ['auto', 'sqrt']
+max_depth = [int(x) for x in np.linspace(10, 110, num=11)]
+max_depth.append(None)
+min_samples_split = [2, 5, 10]
+min_samples_leaf = [1, 2, 4]
+bootstrap = [True, False]
+
+def random_forest_exploring(X, y):
+    random_grid = {'n_estimators': n_estimators,
+                   'max_features': max_features,
+                   'max_depth': max_depth,
+                   'min_samples_split': min_samples_split,
+                   'min_samples_leaf': min_samples_leaf,
+                   'bootstrap': bootstrap}
+
+    rf = RandomForestClassifier()
+
+    rf_random = RandomizedSearchCV(estimator=rf,
+                                   param_distributions=random_grid, n_iter=100,
+                                   cv=3, verbose=2, random_state=42, n_jobs=-1)
+
+    rf_random.fit(X, y)
+
+    rf_random.best_params_# Creating importances_df dataframe
+    importances_df = pd.DataFrame({"feature_names" : rf.feature_names_in_,
+                                   "importances" : rf.feature_importances_})
+
+    # Plotting bar chart, g is from graph
+    g = sns.barplot(x=importances_df["feature_names"],
+                    y=importances_df["importances"])
+    g.set_title("Feature importances", fontsize=14);
--- task_1/code/main.py
+++ task_1/code/main.py
@@ -1,5 +1,33 @@
 # %%
+import sys 
+sys.path.append("./")
 import pandas as pd
+import numpy as np
+import sklearn as sk
+from typing import Optional
+from sklearn.model_selection import RandomizedSearchCV
+from sklearn.ensemble import RandomForestClassifier
+
+# %%
+def preprocess_data(X: pd.DataFrame, y: Optional[pd.Series] = None):
+    """
+    preprocess data
+    Parameters
+    ----------
+    X : DataFrame of shape (n_samples, n_features)
+        Design matrix of regression problem
+
+    y : array-like of shape (n_samples, )
+        Response vector corresponding given samples
+
+    Returns
+    -------
+    Post-processed design matrix and response vector (prices) - either as a single
+    DataFrame or a Tuple[DataFrame, Series]
+    """
+    X = X.drop(["h_booking_id"])
+    X["booking_day_of_year"] = pd.to_datetime(X["booking_datetime"]).dt.dayofyear
+
 
 # %%
 if __name__ == "__main__":
