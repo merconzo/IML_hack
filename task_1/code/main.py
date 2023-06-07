93dec62c529d2c5fcbece6835b2d864b24e8325e
merconzo <75909944+merconzo@users.noreply.github.com>
2023-06-08 00:25:57 +0300

    bools to int

--- task_1/code/main.py
+++ task_1/code/main.py
@@ -1,5 +1,6 @@
 # %%
-import sys 
+import sys
+
 sys.path.append("./")
 import pandas as pd
 import numpy as np
@@ -8,6 +9,7 @@ from typing import Optional
 from sklearn.model_selection import RandomizedSearchCV
 from sklearn.ensemble import RandomForestClassifier
 from hackathon_code.explore_data import random_forest_exploring
+
 
 # %%
 def preprocess_data(X: pd.DataFrame, y: Optional[pd.Series] = None):
@@ -23,19 +25,38 @@ def preprocess_data(X: pd.DataFrame, y: Optional[pd.Series] = None):
 
     Returns
     -------
-    Post-processed design matrix and response vector (prices) - either as a single
+    Post-processed design matrix and response vector (prices) - either as a
+    single
     DataFrame or a Tuple[DataFrame, Series]
     """
-    X = X.drop(["h_booking_id"])
-    X["booking_day_of_year"] = pd.to_datetime(X["booking_datetime"]).dt.dayofyear
+    try:
+        X = X.drop(["h_booking_id"])
+    except:
+        pass
+    X.replace("UNKNOWN", None, inplace=True)
+    X.is_user_logged_in = X.is_user_logged_in.astype(int)
+    X.is_first_booking = X.is_first_booking.astype(int)
+
+    # dates
+    X["booking_day_of_year"] = pd.to_datetime(
+        X["booking_datetime"]).dt.dayofyear
+
+    return X, y
 
 
 # %%
 if __name__ == "__main__":
     np.random.seed(0)
     data = pd.read_csv("./hackathon_code/data/agoda_cancellation_train.csv")
+    cols = data.columns.values
     train, test = sk.model_selection.train_test_split(data, test_size=0.2)
-    X_train, y_train = train.drop(["cancellation_datetime"], axis=1), train["cancellation_datetime"]
-    X_test, y_test = test.drop(["cancellation_datetime"], axis=1), test["cancellation_datetime"]
+    X_train, y_train = train.drop(["cancellation_datetime"], axis=1), train[
+        "cancellation_datetime"]
+    X_test, y_test = test.drop(["cancellation_datetime"], axis=1), test[
+        "cancellation_datetime"]
+    # %%
+    X_train, y_train = preprocess_data(X_train, y_train)
+
+    # %%
     random_forest_exploring(X_train, y_train)
 # %%
