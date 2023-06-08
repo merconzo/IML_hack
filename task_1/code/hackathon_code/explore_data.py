15603c7cc572e2efa77bc3aed7a78d0e20148527
shaypripi <shayprip@gmail.com>
2023-06-08 23:06:10 +0300

    filled data exploration

--- task_1/code/hackathon_code/explore_data.py
+++ task_1/code/hackathon_code/explore_data.py
@@ -1,39 +1,71 @@
 import pandas as pd
 import numpy as np
-import sklearn as sk
-from sklearn.model_selection import RandomizedSearchCV
-from sklearn.ensemble import RandomForestClassifier
 import plotly.express as px
 
 
-n_estimators = [int(x) for x in np.linspace(start=50, stop=2000, num=10)]
-max_features = ['auto', 'sqrt']
-max_depth = [int(x) for x in np.linspace(10, 110, num=11)]
-max_depth.append(None)
-min_samples_split = [2, 5, 10]
-min_samples_leaf = [1, 2, 4]
-bootstrap = [True, False]
+def explore_days_between_booking_and_cancelation(df):
+    canceled_indices = df[df["cancellation_datetime"].apply(lambda x: type(x) == str)]
+    booking_until_cancelation = pd.to_datetime(
+        canceled_indices["cancellation_datetime"]).dt.day_of_year - pd.to_datetime(
+        canceled_indices["booking_datetime"]).dt.day_of_year
+    booking_until_cancelation = booking_until_cancelation.apply(lambda x: x if x > 0 else 0)
+    unique_vals = booking_until_cancelation.unique()
+    unique_vals.sort()
+    # counting the values of booking_until_cancelation
+    counts = [np.sum(booking_until_cancelation == val) for val in unique_vals]
+    # plotting the counts
+    fig = px.bar(pd.DataFrame({"Days since booking": unique_vals, "Number of Cancelations": counts}),
+                 x="Days since booking", y="Number of Cancelations", color="Number of Cancelations"
+                 , title="Days Between Booking and Cancelation")
+    fig.show()
 
-def random_forest_exploring(X, y):
-    random_grid = {'n_estimators': n_estimators,
-                   'max_features': max_features,
-                   'max_depth': max_depth,
-                   'min_samples_split': min_samples_split,
-                   'min_samples_leaf': min_samples_leaf,
-                   'bootstrap': bootstrap}
 
-    rf = RandomForestClassifier()
+def explore_days_between_cancelation_and_checkin(df):
+    canceled_indices = df[df["cancellation_datetime"].apply(lambda x: type(x) == str)]
+    days_till_checkin = pd.to_datetime(canceled_indices["checkin_date"]).dt.day_of_year - pd.to_datetime(
+        canceled_indices["cancellation_datetime"]).dt.day_of_year
+    days_till_checkin = days_till_checkin.apply(lambda x: x if x > 0 else 0)
+    unique_vals = days_till_checkin.unique()
+    unique_vals.sort()
+    # counting the values of booking_until_cancelation
+    counts = [np.sum(days_till_checkin == val) for val in unique_vals]
+    # plotting the counts
+    fig = px.bar(pd.DataFrame({"Days since cancelation till checkin": unique_vals, "Number of Cancelations": counts}),
+                 x="Days since cancelation till checkin", y="Number of Cancelations", color="Number of Cancelations"
+                 , title="Days Between Cancelation and Checkin")
+    fig.show()
 
-    rf_random = RandomizedSearchCV(estimator=rf,
-                                   param_distributions=random_grid, n_iter=100,
-                                   cv=3, verbose=2, random_state=42, n_jobs=-1)
 
-    rf_random.fit(X, y)
+# def interpolate_distance_between_booking_and_checkin(df):
+#     canceled_indices = df[df["cancellation_datetime"].apply(lambda x: type(x) == str)]
+#     days_till_checkin = pd.to_datetime(canceled_indices["checkin_date"]).dt.day_of_year - pd.to_datetime(canceled_indices["cancellation_datetime"]).dt.day_of_year
+#     days_till_checkin = days_till_checkin.apply(lambda x: x if x > 0 else 0)
+#     booking_until_cancelation = pd.to_datetime(canceled_indices["cancellation_datetime"]).dt.day_of_year - pd.to_datetime(canceled_indices["booking_datetime"]).dt.day_of_year
+#     booking_until_cancelation = booking_until_cancelation.apply(lambda x: x if x > 0 else 0)
+#     #interpolating the distance between booking and checkin
+#     distance_between_booking_and_checkin = booking_until_cancelation + days_till_checkin
+#     unique_vals = distance_between_booking_and_checkin.unique()
+#     unique_vals.sort()
+#     #counting the values of booking_until_cancelation
+#     counts = [np.sum(distance_between_booking_and_checkin == val) for val in unique_vals]
+#     #plotting the counts
+#     fig = px.bar(pd.DataFrame({"Distance between booking and checkin": unique_vals, "Number of Cancelations": counts}),
+#                     x="Distance between booking and checkin", y="Number of Cancelations", color="Number of Cancelations"
+#                 , title="Distance Between Booking and Checkin")
+#     fig.show()
 
-    rf_random.best_params_# Creating importances_df dataframe
-    importances_df = pd.DataFrame({"feature_names" : rf.feature_names_in_,
-                                   "importances" : rf.feature_importances_})
-
-    # Plotting bar chart, g is from graph
-    g = px.bar(importances_df, x="feature_names", y="importances", title="Feature importances")
-    g.show()
+def explore_relation_of_booking_and_checkin_difference(df):
+    day_between_booking_and_checkin = pd.to_datetime(df["checkin_date"]).dt.weekofyear - pd.to_datetime(
+        df["booking_datetime"]).dt.weekofyear
+    day_between_booking_and_checkin = day_between_booking_and_checkin.apply(lambda x: x if x >= 0 else x + 52)
+    # getting the mean of cancelation per this data
+    binary_y = df["cancellation_datetime"].apply(lambda x: 1 if type(x) == str else 0)
+    # getting the cancelation rate per day between booking and checkin
+    cancelation_rate = [np.mean(binary_y[day_between_booking_and_checkin == val]) for val in
+                        day_between_booking_and_checkin.unique()]
+    # plotting the cancelation rate
+    fig = px.bar(pd.DataFrame({"Weeks between booking and checkin": day_between_booking_and_checkin.unique(),
+                               "Cancelation Rate": cancelation_rate}),
+                 x="Weeks between booking and checkin", y="Cancelation Rate", color="Cancelation Rate"
+                 , title="Cancelation Rate per Weeks Between Booking and Checkin")
+    fig.show()
