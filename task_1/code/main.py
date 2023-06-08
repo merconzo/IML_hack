6ae0bf8ee97d20393a159c7c6441dba2ab8a804b
merconzo <75909944+merconzo@users.noreply.github.com>
2023-06-08 11:50:54 +0300

    rooms

--- task_1/code/main.py
+++ task_1/code/main.py
@@ -1,7 +1,6 @@
 # %%
-import sys
-
-sys.path.append("./")
+# import sys
+# sys.path.append("./")
 import pandas as pd
 import numpy as np
 import sklearn as sk
@@ -10,9 +9,21 @@ from sklearn.model_selection import RandomizedSearchCV
 from sklearn.ensemble import RandomForestClassifier
 from hackathon_code.explore_data import random_forest_exploring
 
+import plotly.graph_objects as go
+from plotly.subplots import make_subplots
+import plotly.express as px
+
+# typehints
+df = pd.DataFrame
+col = pd.Series
+op_col = Optional[col]
+
+# constants
+Y_COL = "cancellation_datetime"
+
 
 # %%
-def preprocess_data(X: pd.DataFrame, y: Optional[pd.Series] = None):
+def preprocess_data(X: df, y: op_col = None):
     """
     preprocess data
     Parameters
@@ -30,12 +41,16 @@ def preprocess_data(X: pd.DataFrame, y: Optional[pd.Series] = None):
     DataFrame or a Tuple[DataFrame, Series]
     """
     try:
-        X = X.drop(["h_booking_id"])
+         X.drop(["h_booking_id"], inplace=True)
     except:
         pass
-    X.replace("UNKNOWN", None, inplace=True)
+    # X.replace("UNKNOWN", None, inplace=True)
     X.is_user_logged_in = X.is_user_logged_in.astype(int)
     X.is_first_booking = X.is_first_booking.astype(int)
+
+    # rooms
+    X["total_guests_num"] = X.no_of_adults + X.no_of_children
+    X["guests_to_rooms_ration"] = X["total_guests_num"] / X["no_of_room"]
 
     # dates
     X["booking_day_of_year"] = pd.to_datetime(
@@ -50,12 +65,27 @@ if __name__ == "__main__":
     data = pd.read_csv("./hackathon_code/data/agoda_cancellation_train.csv")
     cols = data.columns.values
     train, test = sk.model_selection.train_test_split(data, test_size=0.2)
-    X_train, y_train = train.drop(["cancellation_datetime"], axis=1), train[
-        "cancellation_datetime"]
-    X_test, y_test = test.drop(["cancellation_datetime"], axis=1), test[
-        "cancellation_datetime"]
+    X_train, y_train = train.drop([Y_COL], axis=1), train[
+        Y_COL]
+    X_test, y_test = test.drop([Y_COL], axis=1), test[
+        Y_COL]
     # %%
     X_train, y_train = preprocess_data(X_train, y_train)
+
+    # %% plots
+    px.scatter(X_train, x="no_of_adults", y="no_of_room").show()
+    
+    # go.Figure([
+    #     go.Scatter(x=X_train.no_of_adults,
+    #                y=X_train.no_of_room,
+    #                mode='markers',
+    #                marker=dict(color='purple'),
+    #                name=r'$Loss$')]).update_layout(
+    #     title=f"adults over rooms",
+    #     xaxis=dict(title=f"no_of_adults", showgrid=True),
+    #     yaxis=dict(title=f"no_of_room", showgrid=True)
+    #     ).show()
+    
 
     # %%
     random_forest_exploring(X_train, y_train)
