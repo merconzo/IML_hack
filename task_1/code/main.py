6887a27a2c30b2a7a83fcd671a2209af1ce08b02
merconzo <75909944+merconzo@users.noreply.github.com>
2023-06-09 06:49:59 +0300

    moved docx

--- task_1/code/main.py
+++ task_1/code/main.py
@@ -1,48 +1,49 @@
 # %%
-# import sys
-# sys.path.append("./")
 import pandas as pd
 import numpy as np
-import plotly.graph_objects as go
-
-# %%
-import sklearn as sk
 from joblib import load
-import sklearn.linear_model
+import sklearn as sk
 from sklearn.ensemble import RandomForestClassifier
 from sklearn.pipeline import make_pipeline
 
-from task_1 import execute_task_1, prepare_train_1
-from task_2 import execute_task_2, prepare_train_2
+from task_1.code.hackathon_code.task_1 import execute_task_1, prepare_train_1
+from task_1.code.hackathon_code.task_2 import execute_task_2, prepare_train_2
 
 if __name__ == "__main__":
     np.random.seed(0)
-    # part one
+    ###  part one  ###
     data = pd.read_csv("./hackathon_code/data/agoda_cancellation_train.csv")
-    # model_1 = make_pipeline(sklearn.preprocessing.StandardScaler(),
-    #                         sklearn.ensemble.AdaBoostClassifier(sk.tree.DecisionTreeClassifier(max_depth=2),
-    #                                                             n_estimators=50))
-    # model_2 = make_pipeline(sklearn.preprocessing.StandardScaler(),
-    #                         sklearn.ensemble.HistGradientBoostingRegressor())
-    # prepare_train_1(data, model_1)
-    # prepare_train_2(data, model_2)
-    model_1 = load("model_1.joblib")
-    model_2 = load("model_2.joblib")
+    # the following code is for training the data:
+    model_1 = make_pipeline(sk.preprocessing.StandardScaler(),
+                            sk.ensemble.AdaBoostClassifier(
+                                sk.tree.DecisionTreeClassifier(max_depth=2),
+                                                                n_estimators=50))
+    model_2 = make_pipeline(sk.preprocessing.StandardScaler(),
+                            sk.ensemble.HistGradientBoostingRegressor())
+    prepare_train_1(data, model_1)
+    prepare_train_2(data, model_2)
 
-    # part two
+    # load trained model
+    model_1 = load("hackathon_code/model_1.joblib")
+    model_2 = load("hackathon_code/model_2.joblib")
+
+    ###  part two  ###
+    # task 1
     test_1 = pd.read_csv("./hackathon_code/data/Agoda_Test_1.csv")
     booking_id_1 = test_1["h_booking_id"]
     prediction_1 = execute_task_1(model_1, test_1)
     result_1 = pd.DataFrame({'ID': booking_id_1, 'cancellation': prediction_1})
-    result_1.to_csv("agoda_cancellation_prediction.csv", index=False)
+    result_1.to_csv("task_1/predictions/agoda_cancellation_prediction.csv", index=False)
+
+    # task 2
     test_2 = pd.read_csv("./hackathon_code/data/Agoda_Test_2.csv")
     booking_id_2 = test_2["h_booking_id"]
     test_2 = execute_task_2(model_2, test_2)
     prediction_cancel_2 = execute_task_1(model_1, test_2)
     prediction_2 = test_2["original_selling_amount"].where(prediction_cancel_2 == 1, other=-1)
     result_2 = pd.DataFrame({'ID': booking_id_2, 'predicted_selling_amount': prediction_2})
-    result_2.to_csv("agoda_cost_of_cancellation.csv", index=False)
+    result_2.to_csv("task_1/predictions/agoda_cost_of_cancellation.csv", index=False)
 
-    # part three
+    ###  part three  ###
 
 # %%
