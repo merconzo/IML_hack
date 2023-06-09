dac84f653a7d80ae724c01ea3ffc7835f3ca3118
davidkalech <91252108+davidkalech@users.noreply.github.com>
2023-06-09 04:49:31 +0300

    dummies

--- README.md
+++ README.md
@@ -1,2 +1,13 @@
 # IML_hack
 IML 2023 Hackathon
+files:
+
+main.py - this file contains the main function. it divided into 3 parts.
+part 1 - we upload the two models from the files. if we want to train the model we can 
+make the files that save the models
+part 2 - we open the tests data and preprocess it and predict the missing values
+part 3 - we drew graphs that aggregating the data
+
+task_1 - in these file we have the preprocess of the data for the first task.
+we also have a class that contains the model we want to save after it fit, and usefully data from the train preprocess.
+also, we have prepare_train_1 function that called once when we want to fit the model.
--- task_1/code/main.py
+++ task_1/code/main.py
@@ -9,6 +9,7 @@ import sklearn as sk
 from joblib import load
 import sklearn.linear_model
 from sklearn.ensemble import RandomForestClassifier
+from sklearn.pipeline import make_pipeline
 
 from task_1 import execute_task_1, prepare_train_1
 from task_2 import execute_task_2, prepare_train_2
@@ -16,13 +17,13 @@ from task_2 import execute_task_2, prepare_train_2
 if __name__ == "__main__":
     np.random.seed(0)
     data = pd.read_csv("./hackathon_code/data/agoda_cancellation_train.csv")
-    model_1 = sk.pipeline.make_pipeline(sklearn.preprocessing.StandardScaler(),
-                                        sklearn.ensemble.AdaBoostClassifier(sk.tree.DecisionTreeClassifier(max_depth=1),
-                                                                            n_estimators=50))
-    model_2 = sk.pipeline.make_pipeline(sklearn.preprocessing.StandardScaler(),
-                                        sklearn.ensemble.HistGradientBoostingRegressor())
-    prepare_train_1(data, model_1)
-    prepare_train_2(data, model_2)
+    # model_1 = make_pipeline(sklearn.preprocessing.StandardScaler(),
+    #                         sklearn.ensemble.AdaBoostClassifier(sk.tree.DecisionTreeClassifier(max_depth=1),
+    #                                                             n_estimators=50))
+    # model_2 = make_pipeline(sklearn.preprocessing.StandardScaler(),
+    #                         sklearn.ensemble.HistGradientBoostingRegressor())
+    # prepare_train_1(data, model_1)
+    # prepare_train_2(data, model_2)
     model_1 = load("model_1.joblib")
     model_2 = load("model_2.joblib")
 
