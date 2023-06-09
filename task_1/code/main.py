ef3d1916b1489fafdbd37df1c6dd6177c48e4d3e
davidkalech <91252108+davidkalech@users.noreply.github.com>
2023-06-09 04:22:36 +0300

    dummies

--- task_1/code/main.py
+++ task_1/code/main.py
@@ -3,12 +3,12 @@
 # sys.path.append("./")
 import pandas as pd
 import numpy as np
-import sklearn as sk
-from typing import Optional
-import re
 
 # %%
+import sklearn as sk
 from joblib import load
+import sklearn.linear_model
+from sklearn.ensemble import RandomForestClassifier
 
 from task_1 import execute_task_1, prepare_train_1
 from task_2 import execute_task_2, prepare_train_2
@@ -16,11 +16,15 @@ from task_2 import execute_task_2, prepare_train_2
 if __name__ == "__main__":
     np.random.seed(0)
     data = pd.read_csv("./hackathon_code/data/agoda_cancellation_train.csv")
-    # prepare_train_1(data)
-    # prepare_train_2(data)
+    model_1 = sk.pipeline.make_pipeline(sklearn.preprocessing.StandardScaler(),
+                                        sklearn.ensemble.AdaBoostClassifier(sk.tree.DecisionTreeClassifier(max_depth=1),
+                                                                            n_estimators=50))
+    model_2 = sk.pipeline.make_pipeline(sklearn.preprocessing.StandardScaler(),
+                                        sklearn.ensemble.HistGradientBoostingRegressor())
+    prepare_train_1(data, model_1)
+    prepare_train_2(data, model_2)
     model_1 = load("model_1.joblib")
     model_2 = load("model_2.joblib")
-    # TODO: model two
 
     test_1 = pd.read_csv("./hackathon_code/data/Agoda_Test_1.csv")
     booking_id_1 = test_1["h_booking_id"]
@@ -30,12 +34,9 @@ if __name__ == "__main__":
     test_2 = pd.read_csv("./hackathon_code/data/Agoda_Test_2.csv")
     booking_id_2 = test_2["h_booking_id"]
     test_2 = execute_task_2(model_2, test_2)
-    pd.set_option('display.max_columns', None)  # Show all columns
-    pd.set_option('display.expand_frame_repr', False)  # Disable line breaks
-    print(test_2)
     prediction_cancel_2 = execute_task_1(model_1, test_2)
     prediction_2 = test_2["original_selling_amount"].where(prediction_cancel_2 == 1, other=-1)
     result_2 = pd.DataFrame({'ID': booking_id_2, 'predicted_selling_amount': prediction_2})
     result_2.to_csv("agoda_cost_of_cancellation.csv", index=False)
-    print(result_2)
+
 # %%
